59731a2e88803dfc09af72c27ccbb35f20a92b95
sohal <tosohal@gmail.com>
2026-01-04 19:08:59 +0100

    fix things

--- cmake/deps.nucleo-hal.cmake
+++ cmake/deps.nucleo-hal.cmake
@@ -8,24 +8,33 @@ endif()
 
 # Use GITHUB_BRANCH_hal753 from cache (set in VersionPresets.json)
 if(NOT DEFINED GITHUB_BRANCH_hal753)
-    set(GITHUB_BRANCH_hal753 "v1.0.1" CACHE STRING "hal753 release version")
+    set(GITHUB_BRANCH_hal753 "v1.0.1" CACHE STRING "hal753 release tag")
 endif()
 
-# Derive HAL753_VERSION from GITHUB_BRANCH_hal753 (strip 'v' prefix)
-# Expected format: vX.Y.Z -> X.Y.Z
-string(REGEX REPLACE "^v" "" HAL753_VERSION "${GITHUB_BRANCH_hal753}")
-set(HAL753_VERSION "${HAL753_VERSION}" CACHE STRING "hal753 package version" FORCE)
+# hal753 package version (STM32CubeH7 version, e.g., 1.12.1)
+# This is the version in the package filename, not the release tag
+if(NOT DEFINED GITHUB_BRANCH_hal753_PACKAGE_VERSION)
+    set(GITHUB_BRANCH_hal753_PACKAGE_VERSION "1.12.1" CACHE STRING "hal753 package version")
+endif()
+
+# Extract the CMake package version from GITHUB_BRANCH_hal753 (strip 'v' prefix)
+# This is used for find_package() and must match the version in nucleoConfigVersion.cmake
+string(REGEX REPLACE "^v" "" HAL753_CMAKE_VERSION "${GITHUB_BRANCH_hal753}")
+
+# The package filename uses the STM32CubeH7 version
+set(HAL753_PACKAGE_VERSION "${GITHUB_BRANCH_hal753_PACKAGE_VERSION}")
 
 # Select package URL and checksum based on toolchain
 if(TOOLCHAIN_VARIANT STREQUAL "gnuarm14.3")
-    set(HAL753_PACKAGE_URL "https://github.com/sohal/hal753/releases/download/${GITHUB_BRANCH_hal753}/nucleo-hal-${HAL753_VERSION}-gnuarm14.3.tar.gz")
+    # For testing: use local package file
+    set(HAL753_PACKAGE_URL "https://github.com/sohal/hal753/releases/download/${GITHUB_BRANCH_hal753}/nucleo-hal-${HAL753_PACKAGE_VERSION}-gnuarm14.3.tar.gz")
     if(DEFINED GITHUB_BRANCH_hal753_SHA256)
         set(HAL753_PACKAGE_SHA256 "${GITHUB_BRANCH_hal753_SHA256}")
     else()
-        set(HAL753_PACKAGE_SHA256 "b7763477739c3dc7180293e3fe0d049ff653cb05e1ab6ac69152fe94a5142945")
+        set(HAL753_PACKAGE_SHA256 "bcb77ed9f404ca1c979ca51d6f30b5e66097a0fb43923f62084011427d9a2d8f")
     endif()
 elseif(TOOLCHAIN_VARIANT STREQUAL "atfe21.1")
-    set(HAL753_PACKAGE_URL "https://github.com/sohal/hal753/releases/download/${GITHUB_BRANCH_hal753}/nucleo-hal-${HAL753_VERSION}-atfe21.1.tar.gz")
+    set(HAL753_PACKAGE_URL "https://github.com/sohal/hal753/releases/download/${GITHUB_BRANCH_hal753}/nucleo-hal-${HAL753_PACKAGE_VERSION}-atfe21.1.tar.gz")
     # TODO: Add SHA256 checksum when atfe21.1 package is available
     if(DEFINED GITHUB_BRANCH_hal753_SHA256)
         set(HAL753_PACKAGE_SHA256 "${GITHUB_BRANCH_hal753_SHA256}")
@@ -37,12 +46,12 @@ else()
     message(FATAL_ERROR "Invalid TOOLCHAIN_VARIANT: ${TOOLCHAIN_VARIANT}. Must be 'gnuarm14.3' or 'atfe21.1'")
 endif()
 
-message(STATUS "Fetching hal753 v${HAL753_VERSION} for ${TOOLCHAIN_VARIANT}")
+message(STATUS "Fetching hal753 v${HAL753_PACKAGE_VERSION} for ${TOOLCHAIN_VARIANT}")
 
 # Fetch the hal753 package
 CPMAddPackage(
     NAME nucleo-hal
-    VERSION ${HAL753_VERSION}
+    VERSION ${HAL753_CMAKE_VERSION}
     URL ${HAL753_PACKAGE_URL}
     URL_HASH SHA256=${HAL753_PACKAGE_SHA256}
     DOWNLOAD_ONLY YES
