658d496176e7a6d662ba6c21fbc8dce91734dc46
sohal <tosohal@gmail.com>
2026-01-05 16:03:19 +0100

    fix cmake

--- cmake/deps.toolchain.cmake
+++ cmake/deps.toolchain.cmake
@@ -1,8 +1,11 @@
 # dependency on the toolchain is brought by CPM as a package
-CPMAddPackage(
-  NAME cmake_scripts
-  URL https://github.com/kodezine/cmake_scripts/archive/refs/tags/${GITHUB_BRANCH_toolchain}.tar.gz
-  URL_HASH SHA256=${GITHUB_BRANCH_toolchain_SHA256}
-  OPTIONS
-    DOWNLOAD_ONLY TRUE
-)
+cpmaddpackage(
+    NAME
+    cmake_scripts
+    URL
+    https://github.com/kodezine/cmake_scripts/archive/refs/tags/${GITHUB_BRANCH_toolchain}.tar.gz
+    URL_HASH
+    SHA256=${GITHUB_BRANCH_toolchain_SHA256}
+    OPTIONS
+    DOWNLOAD_ONLY
+    TRUE)
