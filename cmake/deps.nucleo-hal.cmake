f1ed876c80ee451c6f598b362fefe431bbeb3884
sohal <tosohal@gmail.com>
2026-01-05 16:20:06 +0100

    fix paths

--- cmake/deps.nucleo-hal.cmake
+++ cmake/deps.nucleo-hal.cmake
@@ -11,7 +11,7 @@ endif()
 # Use GITHUB_BRANCH_hal753 from cache (set in VersionPresets.json)
 if(NOT DEFINED GITHUB_BRANCH_hal753)
     set(GITHUB_BRANCH_hal753
-        "v1.0.1"
+        "v1.0.4"
         CACHE STRING "hal753 release tag")
 endif()
 
@@ -32,22 +32,23 @@ set(HAL753_PACKAGE_VERSION "${GITHUB_BRANCH_hal753_PACKAGE_VERSION}")
 
 # Select package URL and checksum based on toolchain
 if(TOOLCHAIN_VARIANT STREQUAL "gnuarm14.3")
-    # Use local package file for development/testing
     set(HAL753_PACKAGE_URL
-        "file://${CMAKE_CURRENT_SOURCE_DIR}/../hal753/build/nucleo-h753-hal-local-gnuarm14.3/nucleo-hal-${HAL753_PACKAGE_VERSION}-gnuarm14.3.tar.gz"
+        "https://github.com/sohal/hal753/releases/download/${GITHUB_BRANCH_hal753}/nucleo-hal-${HAL753_PACKAGE_VERSION}-gnuarm14.3.tar.gz"
     )
-    # Skip checksum for local package
-    set(HAL753_PACKAGE_SHA256 "")
-elseif(TOOLCHAIN_VARIANT STREQUAL "atfe21.1")
-    set(HAL753_PACKAGE_URL
-        "https://github.com/sohal/hal753/releases/download/${GITHUB_BRANCH_hal753}/nucleo-hal-${HAL753_PACKAGE_VERSION}-atfe21.1.tar.gz"
-    )
-    # TODO: Add SHA256 checksum when atfe21.1 package is available
     if(DEFINED GITHUB_BRANCH_hal753_SHA256)
         set(HAL753_PACKAGE_SHA256 "${GITHUB_BRANCH_hal753_SHA256}")
     else()
         set(HAL753_PACKAGE_SHA256 "")
-        message(WARNING "atfe21.1 package checksum not yet available")
+    endif()
+elseif(TOOLCHAIN_VARIANT STREQUAL "atfe21.1")
+    # Use gnuarm14.3 package for atfe21.1 builds (atfe21.1 packages not yet available)
+    set(HAL753_PACKAGE_URL
+        "https://github.com/sohal/hal753/releases/download/${GITHUB_BRANCH_hal753}/nucleo-hal-${HAL753_PACKAGE_VERSION}-gnuarm14.3.tar.gz"
+    )
+    if(DEFINED GITHUB_BRANCH_hal753_SHA256)
+        set(HAL753_PACKAGE_SHA256 "${GITHUB_BRANCH_hal753_SHA256}")
+    else()
+        set(HAL753_PACKAGE_SHA256 "")
     endif()
 else()
     message(
